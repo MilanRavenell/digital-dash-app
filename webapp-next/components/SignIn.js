e0fd9acd4d98a8f82cbc8c787ca7658d617d669d
Milan Ravenell <milanravenell@Milans-MacBook-Air-2.local>
2022-09-21 13:29:03 -0700

    stylizing landing screen

--- webapp-next/components/SignIn.js
+++ webapp-next/components/SignIn.js
@@ -5,8 +5,11 @@ import {
     useAuthenticator,
     Authenticator,
 } from "@aws-amplify/ui-react";
+import { platformToLogoUrlMap } from '../helpers';
 
 import styles from '../styles/SignIn.module.css';
+
+const platforms = ['twitter', 'youtube', 'instagram', 'tiktok']
 
 const SignIn = ({}) => {
     const router = useRouter();
@@ -43,7 +46,42 @@ const SignIn = ({}) => {
     return (
         <div className={styles.container}>
             <div className={styles.left}>
-        
+                <div className={styles.contentLeft}>
+                    <div className={styles.title}>
+                        Lightweight platform for tracking analyitcs for all your social media accounts in one place - totally for free!
+                    </div>
+                    <div className={`${styles.icons} ${styles.grow}`}>
+                    {
+                        platforms.map(platform => (
+                            <div className={styles.icon}>
+                                <img
+                                    src={platformToLogoUrlMap[platform].url}
+                                    alt='profile pic'
+                                    style={{
+                                        height: '100%',
+                                        width: '100%',
+                                        objectFit: 'contain',
+                                    }}
+                                    referrerPolicy="no-referrer"
+                                />
+                            </div>
+                        ))
+                    }
+                    </div>
+                    <div className={`${styles.screenshot} ${styles.grow}`}>
+                        <img
+                            src={'/screenshot.png'}
+                            alt='screenshot'
+                            style={{
+                                height: '100%',
+                                width: '100%',
+                                objectFit: 'contain',
+                                borderRadius: '15px',
+                            }}
+                            referrerPolicy="no-referrer"
+                        />
+                    </div>
+                </div>
             </div>
             <div className={styles.right}>
                 <div className={styles.authenticator}>
