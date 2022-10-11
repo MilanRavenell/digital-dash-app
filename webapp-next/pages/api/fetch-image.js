2c8239620923e523aea3acbbd44674745785328b
Milan Ravenell <milanravenell@Milans-MacBook-Air-2.local>
2022-10-11 14:33:46 -0700

    updating log

--- webapp-next/pages/api/fetch-image.js
+++ webapp-next/pages/api/fetch-image.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function handler(req, res) {
     const { url } = req.query;
-    console.log('setting cors on request')
+    console.log('fetching image from backend')
 
     const response = await axios.get(url.replace(/@@@@/g, '&'), { responseType: 'arraybuffer' })
     const buffer = Buffer.from(response.data, 'utf-8');
