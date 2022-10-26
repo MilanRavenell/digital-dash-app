c7dfede5cfcecbf74584852a29d7af54d9c60bd3
Milan Ravenell <milanravenell@Milans-MacBook-Air-2.local>
2022-10-26 00:13:11 -0700

    adding footer css to screens

--- webapp-next/pages/index.js
+++ webapp-next/pages/index.js
@@ -223,7 +223,7 @@ export default function App() {
             profileToRefresh={profileToRefresh}
             isMobile={context.isMobile}
             key={'main'}
-          />
+          />,
       ]
     }
 
