a791b950a14d91153961c235baf563f3c6e2c959
Milan Ravenell <milanravenell@Milans-MacBook-Air-2.local>
2022-10-29 18:17:41 -0700

    adding file

---
+++ webapp-next/helpers/platform-properties.js
@@ -0,0 +1,28 @@
+const platformProperties = Object.freeze({
+    'twitter': {
+        logoUrl: `/twitter_logo.png`,
+        profileBorderColor: '#00acee'
+    },
+    'youtube': {
+        logoUrl: `/youtube_logo.png`,
+        profileBorderColor: '#c4302b'
+    },
+    'instagram': {
+        logoUrl: `/instagram_logo.png`,
+        profileBorderColor: '#cd486b'
+    },
+    'instagram-pro': {
+        logoUrl: `/instagram_logo.png`,
+        profileBorderColor: '#cd486b'
+    },
+    'instagram-basic': {
+        logoUrl: `/instagram_logo.png`,
+        profileBorderColor: '#cd486b'
+    },
+    'tiktok': {
+        logoUrl: `/tik_tok_logo.png`,
+        profileBorderColor: '#000000'
+    },
+});
+
+module.exports = platformProperties;
