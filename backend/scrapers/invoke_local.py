1667cca9e8678f539e7157d4dd947af3525c0d91
Milan Ravenell <milanravenell@Milans-MacBook-Air-2.local>
2022-11-30 16:31:35 -0800

    updating cron scheudle for update-analytics staging

--- backend/scrapers/invoke_local.py
+++ backend/scrapers/invoke_local.py
@@ -2,8 +2,8 @@ from scrape_content import handler
 
 print(handler({
     'platform': 'tiktok',
-    'handle': 'techroastshow',
-    'task': 'process_single_content',
-    'content_to_process': '7163174832136293674',
+    'handle': 'orbrealtimeanalytics',
+    'task': 'full_run',
+    # 'content_to_process': '7163174832136293674',
     'use_tor': False,
 }, {}))
